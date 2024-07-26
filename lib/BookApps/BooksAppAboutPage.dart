e41bcc0ab481d9ef2cec806e6ef06e10b33c1d77
Rasel Hossain Adib <adibrasel.2022@gmail.com>
2024-07-26 13:52:23 +0600

    ok

--- lib/BookApps/BooksAppAboutPage.dart
+++ lib/BookApps/BooksAppAboutPage.dart
@@ -11,7 +11,18 @@ class _BooksAppAboutPageState extends State<BooksAppAboutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Books App About Page"),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Poem Book", style:TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold
+            )),
+            SizedBox(height: 30.0,),
+            Text("Developed by Rasel Hossain Adib"),
+          ],
+        ),
+      )
     );
   }
 }
